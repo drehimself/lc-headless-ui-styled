e2faa5525279ffd096148e82a5d1f7c0e63014c1
Andre Madarang <andre_madarang@hotmail.com>
2021-08-12 22:59:03 -0400

    Episode 1 - Menu Dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,125 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="min-h-screen bg-gray-200 text-gray-900">
+    <div class="container mx-auto px-4">
+      <h2 class="font-bold text-xl pt-2">Menu (Dropdown)</h2>
+      <div class="mt-4">
+        <Menu>
+          <MenuButton class="flex items-center bg-blue-500 text-white rounded-md px-4 py-2">
+            <span>More</span>
+            <ChevronDownIcon class="h-5 w-5 ml-2" />
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg> -->
+          </MenuButton>
+
+          <transition
+            enter-active-class="transition duration-200 ease-out"
+            enter-from-class="transform scale-75 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-100 ease-out"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-75 opacity-0"
+          >
+          <MenuItems class="absolute bg-white shadow-md rounded-lg flex flex-col focus:outline-none w-56 divide-y divide-gray-100 origin-top-left mt-2">
+            <div class="px-1 py-1">
+              <MenuItem v-slot="{ active }">
+                <button
+                  class="w-full flex items-center rounded-md px-2 py-2"
+                  :class='{ "bg-blue-500 text-white": active }'
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                  </svg>
+                  <span class="ml-2">Edit</span>
+                </button>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+                <button
+                  class="w-full flex items-center rounded-md px-2 py-2"
+                  :class='{ "bg-blue-500 text-white": active }'
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                  </svg>
+                  <span class="ml-2">Duplicate</span>
+                </button>
+              </MenuItem>
+            </div>
+
+            <div class="px-1 py-1">
+              <MenuItem v-slot="{ active }">
+                <button
+                  class="w-full flex items-center rounded-md px-2 py-2"
+                  :class='{ "bg-blue-500 text-white": active }'
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                  </svg>
+                  <span class="ml-2">Archive</span>
+                </button>
+              </MenuItem>
+
+              <MenuItem v-slot="{ active }">
+                <button
+                  class="w-full flex items-center rounded-md px-2 py-2"
+                  :class='{ "bg-blue-500 text-white": active }'
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" />
+                  </svg>
+                  <span class="ml-2">Move</span>
+                </button>
+              </MenuItem>
+            </div>
+
+            <div class="px-1 py-1">
+              <MenuItem v-slot="{ active, disabled }" disabled>
+                <button
+                  class="w-full flex items-center rounded-md px-2 py-2"
+                  :class='{ "bg-blue-500 text-white": active, "opacity-50 cursor-not-allowed" : disabled }'
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <span class="ml-2">Destroy</span>
+                </button>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+                <button
+                  class="w-full flex items-center rounded-md px-2 py-2"
+                  :class='{ "bg-blue-500 text-white": active }'
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <span class="ml-2">Delete</span>
+                </button>
+              </MenuItem>
+            </div>
+          </MenuItems>
+          </transition>
+        </Menu>
+
+        <div class="mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, incidunt!
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+  import { ChevronDownIcon } from '@heroicons/vue/solid'
+
+  export default {
+    components: {
+      Menu,
+      MenuButton,
+      MenuItems,
+      MenuItem,
+      ChevronDownIcon,
+    },
+  };
 </script>
