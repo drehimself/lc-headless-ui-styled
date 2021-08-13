ac105908e6a343af8a1953f47591cf92b7c63930
Andre Madarang <andre_madarang@hotmail.com>
2021-08-13 00:39:49 -0400

    Episode 2 - Dialog (Modals)

--- src/App.vue
+++ src/App.vue
@@ -104,12 +104,88 @@
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, incidunt!
         </div>
       </div>
+      <div class="mt-4">
+        <h2 class="font-bold text-xl pt-2">Dialog (Modal)</h2>
+        <div class="mt-4">
+          <button @click="openModal" class="flex items-center bg-blue-500 text-white rounded-md px-4 py-2">Open Modal</button>
+
+          <TransitionRoot appear :show="isOpen" as="template">
+            <Dialog as="div" @close="closeModal">
+              <div class="fixed inset-0 z-10 overflow-y-auto">
+                <div class="min-h-screen px-4 text-center">
+                  <TransitionChild
+                    as="template"
+                    enter="duration-300 ease-out"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="duration-200 ease-in"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
+                  >
+                    <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                  </TransitionChild>
+
+                  <span class="inline-block h-screen align-middle" aria-hidden="true">
+                    &#8203;
+                  </span>
+
+                  <TransitionChild
+                    as="template"
+                    enter="duration-300 ease-out"
+                    enter-from="opacity-0 scale-95 rotate-180"
+                    enter-to="opacity-100 scale-100 rotate-0"
+                    leave="duration-200 ease-in"
+                    leave-from="opacity-100 scale-100"
+                    leave-to="opacity-0 scale-95"
+                  >
+                    <div
+                      class="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl"
+                    >
+                      <button @click="closeModal" class="absolute top-3 right-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                      </button>
+                      <DialogTitle
+                        as="h3"
+                        class="text-lg font-medium leading-6 text-gray-900"
+                      >
+                        Payment successful for your order
+                      </DialogTitle>
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-500">
+                          Your payment has been successfully submitted. We’ve sent your
+                          an email with all of the details of your order. lorem400
+                        </p>
+                      </div>
+
+                      <div class="mt-4">
+                        <button
+                          type="button"
+                          class="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                          @click="closeModal"
+                        >
+                          Got it, thanks!
+                        </button>
+                      </div>
+                    </div>
+                  </TransitionChild>
+                </div>
+              </div>
+            </Dialog>
+          </TransitionRoot>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+  import { Menu, MenuButton, MenuItems, MenuItem, TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogOverlay,
+  DialogTitle, } from "@headlessui/vue";
 
   import { ChevronDownIcon } from '@heroicons/vue/solid'
 
@@ -120,6 +196,25 @@
       MenuItems,
       MenuItem,
       ChevronDownIcon,
+      TransitionRoot,
+      TransitionChild,
+      Dialog,
+      DialogOverlay,
+      DialogTitle,
     },
+
+    data() {
+      return {
+        isOpen: false,
+      }
+    },
+    methods: {
+      closeModal() {
+        this.isOpen = false
+      },
+      openModal() {
+        this.isOpen = true
+      },
+    }
   };
 </script>
